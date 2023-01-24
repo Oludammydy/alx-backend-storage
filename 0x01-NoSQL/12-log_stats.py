af1d63f17237d10a2c3e93e19ff65409190c501a
Oluwadamilola Olaniyi <96773767+Oludammydy@users.noreply.github.com>
2023-01-25 00:29:06 +0100

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-script that provides some stats about Nginx logs stored in MongoDB
+Script that provides some stats about Nginx logs stored in MongoDB
 """
+
 from pymongo import MongoClient
 
 
